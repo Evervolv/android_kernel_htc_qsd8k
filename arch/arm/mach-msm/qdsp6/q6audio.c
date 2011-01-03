937f0a8dd3a5ee13e3feacf282d715d49a1c3b0c
Phani Kumar Allada <pallad@codeaurora.org>
2011-01-03 13:14:24 +0530

    msm: audio: 8k: Send ACDB configuration during device switch
    In the current code, for a device switch command, ACDB configuration
    is sent to Q6 only if there is any active tx/rx session.
    As per Q6 API, changing the code to send ACDB configuration after
    the PREPARE stage of the device switch even if there's no active
    tx/rx session.
    
    Change-Id: I895f5da5d23f586ae829c4856a5b39b4399422ee
    CRs-Fixed: 267703
    Signed-off-by: Phani Kumar Allada <pallad@codeaurora.org>

--- arch/arm/mach-msm/qdsp6/q6audio.c
+++ arch/arm/mach-msm/qdsp6/q6audio.c
@@ -1324,6 +1324,11 @@ static void do_rx_routing(uint32_t device_id, uint32_t acdb_id)
 		_audio_rx_clk_reinit(device_id);
 		_audio_rx_path_enable(1, acdb_id);
 	} else {
+		qdsp6_devchg_notify(ac_control, ADSP_AUDIO_RX_DEVICE,
+					device_id);
+		audio_update_acdb(device_id, acdb_id);
+		qdsp6_standby(ac_control);
+		qdsp6_start(ac_control);
 		audio_rx_device_id = device_id;
 		audio_rx_path_id = q6_device_to_path(device_id);
 	}
@@ -1333,7 +1338,8 @@ static void do_tx_routing(uint32_t device_id, uint32_t acdb_id)
 {
 	if (device_id == audio_tx_device_id) {
 		if (acdb_id != tx_acdb) {
-			qdsp6_devchg_notify(ac_control, ADSP_AUDIO_TX_DEVICE, device_id);
+			qdsp6_devchg_notify(ac_control, ADSP_AUDIO_TX_DEVICE,
+						device_id);
 			audio_update_acdb(device_id, acdb_id);
 			qdsp6_standby(ac_control);
 			qdsp6_start(ac_control);
@@ -1347,6 +1353,11 @@ static void do_tx_routing(uint32_t device_id, uint32_t acdb_id)
 		_audio_tx_clk_reinit(device_id);
 		_audio_tx_path_enable(1, acdb_id);
 	} else {
+		qdsp6_devchg_notify(ac_control, ADSP_AUDIO_TX_DEVICE,
+					device_id);
+		audio_update_acdb(device_id, acdb_id);
+		qdsp6_standby(ac_control);
+		qdsp6_start(ac_control);
 		audio_tx_device_id = device_id;
 		audio_tx_path_id = q6_device_to_path(device_id);
 	}
