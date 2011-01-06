1a4c6d888ddf6d803c00a20f1c8f920395df23b9
Dima Zavin <dima@android.com>
2011-01-05 17:01:30 -0800

    ARM: msm: qdsp6: convert audio ioctl to unlocked_ioctl
    Change-Id: I29351cc0091405ebf8b370eda12310cb7a2fabd5
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/qdsp6/audio_ctl.c
+++ arch/arm/mach-msm/qdsp6/audio_ctl.c
@@ -79,8 +79,7 @@ static int q6_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static int q6_ioctl(struct inode *inode, struct file *file,
-		    unsigned int cmd, unsigned long arg)
+static long q6_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	int rc;
 	uint32_t n;
@@ -158,7 +157,7 @@ static int q6_release(struct inode *inode, struct file *file)
 static struct file_operations q6_dev_fops = {
 	.owner		= THIS_MODULE,
 	.open		= q6_open,
-	.ioctl		= q6_ioctl,
+	.unlocked_ioctl		= q6_ioctl,
 	.release	= q6_release,
 };
 
