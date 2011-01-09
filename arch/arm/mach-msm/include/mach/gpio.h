c0748e79eae640aba8e5bb8beb10cbd2b0ae8951
Dima Zavin <dima@android.com>
2011-01-09 13:35:35 -0800

    Revert "[ARM] msm: gpio: Add GPIO_CFG_ prefix to GPIO_CFG argument to avoid conflict with proc_comm.h"
    This reverts commit 5736cd87409d3ee32ba0d424fbfaf950c45f434b.

--- arch/arm/mach-msm/include/mach/gpio.h
+++ arch/arm/mach-msm/include/mach/gpio.h
@@ -21,57 +21,6 @@
 
 #define FIRST_BOARD_GPIO	NR_GPIO_IRQS
 
-/* GPIO TLMM (Top Level Multiplexing) Definitions */
-
-/* GPIO TLMM: Function -- GPIO specific */
-
-/* GPIO TLMM: Direction */
-enum {
-	GPIO_CFG_INPUT,
-	GPIO_CFG_OUTPUT,
-};
-
-/* GPIO TLMM: Pullup/Pulldown */
-enum {
-	GPIO_CFG_NO_PULL,
-	GPIO_CFG_PULL_DOWN,
-	GPIO_CFG_KEEPER,
-	GPIO_CFG_PULL_UP,
-};
-
-/* GPIO TLMM: Drive Strength */
-enum {
-	GPIO_CFG_2MA,
-	GPIO_CFG_4MA,
-	GPIO_CFG_6MA,
-	GPIO_CFG_8MA,
-	GPIO_CFG_10MA,
-	GPIO_CFG_12MA,
-	GPIO_CFG_14MA,
-	GPIO_CFG_16MA,
-};
-
-enum {
-	GPIO_CFG_ENABLE,
-	GPIO_CFG_DISABLE,
-};
-
-#define GPIO_CFG(gpio, func, dir, pull, drvstr) \
-	((((gpio) & 0x3FF) << 4)        |	  \
-	 ((func) & 0xf)                  |	  \
-	 (((dir) & 0x1) << 14)           |	  \
-	 (((pull) & 0x3) << 15)          |	  \
-	 (((drvstr) & 0xF) << 17))
-
-/**
- * extract GPIO pin from bit-field used for gpio_tlmm_config
- */
-#define GPIO_PIN(gpio_cfg)    (((gpio_cfg) >>  4) & 0x3ff)
-#define GPIO_FUNC(gpio_cfg)   (((gpio_cfg) >>  0) & 0xf)
-#define GPIO_DIR(gpio_cfg)    (((gpio_cfg) >> 14) & 0x1)
-#define GPIO_PULL(gpio_cfg)   (((gpio_cfg) >> 15) & 0x3)
-#define GPIO_DRVSTR(gpio_cfg) (((gpio_cfg) >> 17) & 0xf)
-
 #define gpio_get_value  __gpio_get_value
 #define gpio_set_value  __gpio_set_value
 #define gpio_cansleep   __gpio_cansleep
