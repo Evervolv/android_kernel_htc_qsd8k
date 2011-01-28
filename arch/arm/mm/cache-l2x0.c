5832fa79446027d1218c77eae54172ab516ef0ee
Laura Abbott <lauraa@codeaurora.org>
2011-01-28 13:13:47 +0800

    ARM: mm: cache-l2x0: Correct l2x0 initialization
    This corrects the regression in the msm-2.6.35 version
    of this file. The initialization routine was not setting
    L2X0_AUX_CTRL and missing an INV_WAY flush as specified
    in the tech documents.

--- arch/arm/mm/cache-l2x0.c
+++ arch/arm/mm/cache-l2x0.c
@@ -302,8 +302,9 @@ void __init l2x0_init(void __iomem *base, __u32 aux_val, __u32 aux_mask)
 		type = "L2x0 series";
 		break;
 	}
-
+	writel(aux, l2x0_base + L2X0_AUX_CTRL);
 	l2x0_way_mask = (1 << ways) - 1;
+	l2x0_inv_all();
 
 	/*
 	 * L2 cache Size =  Way size * Number of ways
