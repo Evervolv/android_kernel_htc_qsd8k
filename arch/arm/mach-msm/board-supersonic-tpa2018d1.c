d63e9c1f08db2704df2054cfa3f5b2938c391dad
Steve Kondik <shade@chemlab.org>
2011-03-15 13:18:18 -0400

    supersonic: Update tpa2018d1 AMP driver
    Also fix ioctl signature

--- arch/arm/mach-msm/board-supersonic-tpa2018d1.c
+++ arch/arm/mach-msm/board-supersonic-tpa2018d1.c
@@ -1,6 +1,6 @@
-/* drivers/i2c/chips/tpa2018d1.c
+/* driver/i2c/chip/tap2018d1.c
  *
- * TI TPA2018D1 Speaker Amplifier
+ * TI TPA2018D1 Speaker Amp
  *
  * Copyright (C) 2009 HTC Corporation
  *
@@ -15,48 +15,46 @@
  *
  */
 
-/* TODO: content validation in TPA2018_SET_CONFIG */
-
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/device.h>
-#include <linux/fs.h>
+#include <linux/interrupt.h>
 #include <linux/i2c.h>
+#include <linux/slab.h>
+#include <linux/irq.h>
 #include <linux/miscdevice.h>
 #include <linux/gpio.h>
 #include <asm/uaccess.h>
 #include <linux/delay.h>
+#include <linux/input.h>
+#include <linux/workqueue.h>
+#include <linux/freezer.h>
 #include <linux/mutex.h>
-#include <linux/slab.h>
 #include <linux/tpa2018d1.h>
 
 #include "board-supersonic-tpa2018d1.h"
 
+#define DEBUG (0)
+
 static struct i2c_client *this_client;
 static struct tpa2018d1_platform_data *pdata;
-static int is_on;
-static char spk_amp_cfg[8];
-static const char spk_amp_on[8] = { /* same length as spk_amp_cfg */
-	0x01, 0xc3, 0x20, 0x01, 0x00, 0x08, 0x1a, 0x21
-};
-static const char spk_amp_off[] = {0x01, 0xa2};
 
-static DEFINE_MUTEX(spk_amp_lock);
+struct mutex spk_amp_lock;
 static int tpa2018d1_opened;
+static int last_spkamp_state;
+static char SPK_AMP_CFG[8];
+static char DEFAULT_SPK_AMP_ON[] =
+			{0x01, 0xc3, 0x20, 0x01, 0x00, 0x08, 0x1a, 0x21};
+static char SPK_AMP_0FF[] = {0x01, 0xa2};
 static char *config_data;
 static int tpa2018d1_num_modes;
 
-#define DEBUG 0
-
-static int tpa2018_i2c_write(const char *txData, int length)
+static int tpa2018_i2c_write(char *txData, int length)
 {
 	struct i2c_msg msg[] = {
 		{
-			.addr = this_client->addr,
-			.flags = 0,
-			.len = length,
-			.buf = txData,
-		},
+		 .addr = this_client->addr,
+		 .flags = 0,
+		 .len = length,
+		 .buf = txData,
+		 },
 	};
 
 	if (i2c_transfer(this_client->adapter, msg, 1) < 0) {
@@ -68,6 +66,7 @@ static int tpa2018_i2c_write(const char *txData, int length)
 
 static int tpa2018_i2c_read(char *rxData, int length)
 {
+	int rc;
 	struct i2c_msg msgs[] = {
 		{
 			.addr = this_client->addr,
@@ -77,18 +76,18 @@ static int tpa2018_i2c_read(char *rxData, int length)
 		},
 	};
 
-	if (i2c_transfer(this_client->adapter, msgs, 1) < 0) {
-		pr_err("%s: I2C transfer error\n", __func__);
-		return -EIO;
+	rc = i2c_transfer(this_client->adapter, msgs, 1);
+	if (rc < 0) {
+		pr_err("%s: transfer error %d\n", __func__, rc);
+		return rc;
 	}
 
 #if DEBUG
-	do {
+	{
 		int i = 0;
 		for (i = 0; i < length; i++)
-			pr_info("%s: rx[%d] = %2x\n",
-				__func__, i, rxData[i]);
-	} while(0);
+			pr_info("%s: rx[%d] = %2x\n", __func__, i, rxData[i]);
+	}
 #endif
 
 	return 0;
@@ -121,125 +120,117 @@ static int tpa2018d1_release(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static int tpa2018d1_read_config(void __user *argp)
+void tpa2018d1_set_speaker_amp(int on)
 {
-	int rc = 0;
-	unsigned char reg_idx = 0x01;
-	unsigned char tmp[7];
-
-	if (!is_on) {
+	mutex_lock(&spk_amp_lock);
+	if (on && !last_spkamp_state) {
 		gpio_set_value(pdata->gpio_tpa2018_spk_en, 1);
-		msleep(5); /* According to TPA2018D1 Spec */
+		mdelay(5); /* According to TPA2018D1 Spec */
+		if (tpa2018_i2c_write(SPK_AMP_CFG, sizeof(SPK_AMP_CFG)) == 0) {
+			last_spkamp_state = 1;
+			pr_info("%s: ON, value = %x %x\n", __func__, SPK_AMP_CFG[0], SPK_AMP_CFG[1]);
+		}
+	} else if (!on && last_spkamp_state) {
+		if (tpa2018_i2c_write(SPK_AMP_0FF, sizeof(SPK_AMP_0FF)) == 0) {
+			last_spkamp_state = 0;
+			mdelay(2);
+			gpio_set_value(pdata->gpio_tpa2018_spk_en, 0);
+			pr_info("%s: OFF\n", __func__);
+		}
 	}
-
-	rc = tpa2018_i2c_write(&reg_idx, sizeof(reg_idx));
-	if (rc < 0)
-		goto err;
-
-	rc = tpa2018_i2c_read(tmp, sizeof(tmp));
-	if (rc < 0)
-		goto err;
-
-	if (copy_to_user(argp, &tmp, sizeof(tmp)))
-		rc = -EFAULT;
-
-err:
-	if (!is_on)
-		gpio_set_value(pdata->gpio_tpa2018_spk_en, 0);
-	return rc;
+	mutex_unlock(&spk_amp_lock);
 }
 
-static int tpa2018d1_ioctl(struct inode *inode, struct file *file,
-		unsigned int cmd, unsigned long arg)
+static long
+tpa2018d1_ioctl(struct file *file, unsigned int cmd,
+	   unsigned long arg)
 {
 	void __user *argp = (void __user *)arg;
 	int rc = 0;
+	unsigned char tmp[7];
 	int mode = -1;
 	int offset = 0;
+	unsigned char reg_idx[1] = {0x01};
 	struct tpa2018d1_config_data cfg;
-
-	mutex_lock(&spk_amp_lock);
 
 	switch (cmd) {
 	case TPA2018_SET_CONFIG:
-		if (copy_from_user(spk_amp_cfg, argp, sizeof(spk_amp_cfg)))
-			rc = -EFAULT;
+		if (copy_from_user(SPK_AMP_CFG, argp, sizeof(SPK_AMP_CFG)))
+		/* TODO: content validation? */
 		break;
-
-	case TPA2018_READ_CONFIG:
-		rc = tpa2018d1_read_config(argp);
-		break;
-
 	case TPA2018_SET_MODE:
-		if (copy_from_user(&mode, argp, sizeof(mode))) {
-			rc = -EFAULT;
-			break;
-		}
+		if (copy_from_user(&mode, argp, sizeof(mode)))
+			return -EFAULT;
 		if (mode >= tpa2018d1_num_modes || mode < 0) {
-			pr_err("%s: unsupported tpa2018d1 mode %d\n",
-				__func__, mode);
-			rc = -EINVAL;
-			break;
+			pr_err("unsupported tpa2018d1 mode %d\n", mode);
+			return -EINVAL;
 		}
-		if (!config_data) {
-			pr_err("%s: no config data!\n", __func__);
-			rc = -EIO;
-			break;
-		}
-		memcpy(spk_amp_cfg, config_data + mode * TPA2018D1_CMD_LEN,
-			TPA2018D1_CMD_LEN);
+		memcpy(SPK_AMP_CFG, config_data + mode * TPA2018D1_CMD_LEN,
+				TPA2018D1_CMD_LEN);
 		break;
+	case TPA2018_READ_CONFIG:
+		mutex_lock(&spk_amp_lock);
+		if (!last_spkamp_state) {
+			gpio_set_value(pdata->gpio_tpa2018_spk_en, 1);
+			mdelay(5); /* According to TPA2018D1 Spec */
+		}
 
+		rc = tpa2018_i2c_write(reg_idx, sizeof(reg_idx));
+		if (rc < 0)
+			goto err;
+
+		rc = tpa2018_i2c_read(tmp, sizeof(tmp));
+		if (rc < 0)
+			goto err;
+
+		if (copy_to_user(argp, &tmp, sizeof(tmp)))
+			rc = -EFAULT;
+err:
+		if (!last_spkamp_state)
+			gpio_set_value(pdata->gpio_tpa2018_spk_en, 0);
+		mutex_unlock(&spk_amp_lock);
+		break;
 	case TPA2018_SET_PARAM:
+		cfg.mode_num = 0;
+		cfg.cmd_data = 0;
 		if (copy_from_user(&cfg, argp, sizeof(cfg))) {
 			pr_err("%s: copy from user failed.\n", __func__);
-			rc = -EFAULT;
-			break;
+			return -EFAULT;
 		}
 		tpa2018d1_num_modes = cfg.mode_num;
 		if (tpa2018d1_num_modes > TPA2018_NUM_MODES) {
 			pr_err("%s: invalid number of modes %d\n", __func__,
-				tpa2018d1_num_modes);
-			rc = -EINVAL;
-			break;
+					tpa2018d1_num_modes);
+			return -EINVAL;
 		}
 		if (cfg.data_len != tpa2018d1_num_modes*TPA2018D1_CMD_LEN) {
-			pr_err("%s: invalid data length %d, expecting %d\n",
-				__func__, cfg.data_len,
-				tpa2018d1_num_modes * TPA2018D1_CMD_LEN);
-			rc = -EINVAL;
-			break;
+				pr_err("%s: invalid data length %d, expecting %d\n",
+						__func__, cfg.data_len,
+						tpa2018d1_num_modes * TPA2018D1_CMD_LEN);
+				return -EINVAL;
 		}
-		/* Free the old data */
-		if (config_data)
-			kfree(config_data);
 		config_data = kmalloc(cfg.data_len, GFP_KERNEL);
 		if (!config_data) {
 			pr_err("%s: out of memory\n", __func__);
-			rc = -ENOMEM;
-			break;
+			return -ENOMEM;
 		}
 		if (copy_from_user(config_data, cfg.cmd_data, cfg.data_len)) {
 			pr_err("%s: copy data from user failed.\n", __func__);
 			kfree(config_data);
-			config_data = NULL;
-			rc = -EFAULT;
-			break;
+			return -EFAULT;
 		}
 		/* replace default setting with playback setting */
 		if (tpa2018d1_num_modes >= TPA2018_MODE_PLAYBACK) {
 			offset = TPA2018_MODE_PLAYBACK * TPA2018D1_CMD_LEN;
-			memcpy(spk_amp_cfg, config_data + offset,
+			memcpy(SPK_AMP_CFG, config_data + offset,
 					TPA2018D1_CMD_LEN);
 		}
 		break;
-
 	default:
-		pr_err("%s: invalid command %d\n", __func__, _IOC_NR(cmd));
+		pr_err("%s: Invalid command\n", __func__);
 		rc = -EINVAL;
 		break;
 	}
-	mutex_unlock(&spk_amp_lock);
 	return rc;
 }
 
@@ -256,42 +247,19 @@ static struct miscdevice tpa2018d1_device = {
 	.fops = &tpa2018d1_fops,
 };
 
-void tpa2018d1_set_speaker_amp(int on)
-{
-	if (!pdata) {
-		pr_err("%s: no platform data!\n", __func__);
-		return;
-	}
-	mutex_lock(&spk_amp_lock);
-	if (on && !is_on) {
-		gpio_set_value(pdata->gpio_tpa2018_spk_en, 1);
-		msleep(5); /* According to TPA2018D1 Spec */
-
-		if (tpa2018_i2c_write(spk_amp_cfg, sizeof(spk_amp_cfg)) == 0) {
-			is_on = 1;
-			pr_info("%s: ON\n", __func__);
-		}
-	} else if (!on && is_on) {
-		if (tpa2018_i2c_write(spk_amp_off, sizeof(spk_amp_off)) == 0) {
-			is_on = 0;
-			msleep(2);
-			gpio_set_value(pdata->gpio_tpa2018_spk_en, 0);
-			pr_info("%s: OFF\n", __func__);
-		}
-	}
-	mutex_unlock(&spk_amp_lock);
-}
-
-static int tpa2018d1_probe(struct i2c_client *client, const struct i2c_device_id *id)
+int tpa2018d1_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	int ret = 0;
 
 	pdata = client->dev.platform_data;
 
-	if (!pdata) {
-		ret = -EINVAL;
-		pr_err("%s: platform data is NULL\n", __func__);
-		goto err_no_pdata;
+	if (pdata == NULL) {
+		pdata = kzalloc(sizeof(*pdata), GFP_KERNEL);
+		if (pdata == NULL) {
+			ret = -ENOMEM;
+			pr_err("%s: platform data is NULL\n", __func__);
+			goto err_alloc_data_failed;
+		}
 	}
 
 	this_client = client;
@@ -299,20 +267,20 @@ static int tpa2018d1_probe(struct i2c_client *client, const struct i2c_device_id
 	ret = gpio_request(pdata->gpio_tpa2018_spk_en, "tpa2018");
 	if (ret < 0) {
 		pr_err("%s: gpio request aud_spk_en pin failed\n", __func__);
-		goto err_free_gpio;
+		goto err_free_gpio_all;
 	}
 
 	ret = gpio_direction_output(pdata->gpio_tpa2018_spk_en, 1);
 	if (ret < 0) {
 		pr_err("%s: request aud_spk_en gpio direction failed\n",
 			__func__);
-		goto err_free_gpio;
+		goto err_free_gpio_all;
 	}
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
 		pr_err("%s: i2c check functionality error\n", __func__);
 		ret = -ENODEV;
-		goto err_free_gpio;
+		goto err_free_gpio_all;
 	}
 
 	gpio_set_value(pdata->gpio_tpa2018_spk_en, 0); /* Default Low */
@@ -320,15 +288,23 @@ static int tpa2018d1_probe(struct i2c_client *client, const struct i2c_device_id
 	ret = misc_register(&tpa2018d1_device);
 	if (ret) {
 		pr_err("%s: tpa2018d1_device register failed\n", __func__);
-		goto err_free_gpio;
+		goto err_free_gpio_all;
 	}
-	memcpy(spk_amp_cfg, spk_amp_on, sizeof(spk_amp_on));
+	memcpy(SPK_AMP_CFG, DEFAULT_SPK_AMP_ON, sizeof(DEFAULT_SPK_AMP_ON));
 	return 0;
 
-err_free_gpio:
+err_free_gpio_all:
 	gpio_free(pdata->gpio_tpa2018_spk_en);
-err_no_pdata:
+err_alloc_data_failed:
 	return ret;
+}
+
+static int tpa2018d1_remove(struct i2c_client *client)
+{
+	struct tpa2018d1_platform_data *p2018data = i2c_get_clientdata(client);
+	kfree(p2018data);
+
+	return 0;
 }
 
 static int tpa2018d1_suspend(struct i2c_client *client, pm_message_t mesg)
@@ -348,6 +324,7 @@ static const struct i2c_device_id tpa2018d1_id[] = {
 
 static struct i2c_driver tpa2018d1_driver = {
 	.probe = tpa2018d1_probe,
+	.remove = tpa2018d1_remove,
 	.suspend = tpa2018d1_suspend,
 	.resume = tpa2018d1_resume,
 	.id_table = tpa2018d1_id,
@@ -359,10 +336,17 @@ static struct i2c_driver tpa2018d1_driver = {
 static int __init tpa2018d1_init(void)
 {
 	pr_info("%s\n", __func__);
+	mutex_init(&spk_amp_lock);
 	return i2c_add_driver(&tpa2018d1_driver);
 }
 
-module_init(tpa2018d1_init);
+static void __exit tpa2018d1_exit(void)
+{
+	i2c_del_driver(&tpa2018d1_driver);
+}
 
-MODULE_DESCRIPTION("tpa2018d1 speaker amp driver");
+module_init(tpa2018d1_init);
+module_exit(tpa2018d1_exit);
+
+MODULE_DESCRIPTION("TPA2018D1 Speaker Amp driver");
 MODULE_LICENSE("GPL");
