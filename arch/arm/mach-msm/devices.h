rmcc/commtiva-kernel-z71

qup_i2c: Add clock, resources entries for QUP i2c mini-core device
Signed-off-by: Sagar Dharia <sdharia@quicinc.com>

arch/arm/mach-msm/devices.h
@@ -43,6 +43,8 @@ extern struct platform_device msm_device_i2c;
 
 extern struct platform_device msm_device_i2c_2;
 
+extern struct platform_device qup_device_i2c;
+
 extern struct platform_device msm_device_smd;
 extern struct platform_device msm_device_dmov;
 

Evervolv/android_kernel_htc_qsd8k

Change-Id: Iab948cdecbc8155b8c00c4d25c0150e71b9a0bab

arch/arm/mach-msm/devices.h
@@ -37,6 +37,8 @@ extern struct platform_device msm_device_hsusb_host;
 extern struct platform_device msm_device_i2c;
 extern struct platform_device msm_device_i2c2;
 
+extern struct platform_device msm_device_qup_i2c;
+
 extern struct platform_device msm_device_smd;
 
 extern struct platform_device msm_device_nand;