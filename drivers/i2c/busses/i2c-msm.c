360a66e7289baa8193d9f381e75420665a3aebe8
Steve Kondik <shade@chemlab.org>
2011-03-29 23:15:52 -0400

    msm: i2c: Use usleep_range

--- drivers/i2c/busses/i2c-msm.c
+++ drivers/i2c/busses/i2c-msm.c
@@ -254,7 +254,7 @@ msm_i2c_poll_notbusy(struct msm_i2c_dev *dev, int warn)
 			return 0;
 		}
 		if (retries++ > 100)
-			msleep(10);
+			usleep_range(100, 200);
 	}
 	dev_err(dev->dev, "Error waiting for notbusy (%d)\n", warn);
 	return -ETIMEDOUT;
@@ -299,7 +299,7 @@ msm_i2c_recover_bus_busy(struct msm_i2c_dev *dev)
 		gpio_direction_input(gpio_clk);
 		udelay(5);
 		if (!gpio_get_value(gpio_clk))
-			udelay(20);
+			usleep_range(20, 30);
 		if (!gpio_get_value(gpio_clk))
 			msleep(10);
 		gpio_clk_status = gpio_get_value(gpio_clk);
