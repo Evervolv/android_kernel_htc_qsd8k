0576b33882592a449f40f032040808cd9a2b4cc5
toastcfh <toastcfh@gmail.com>
2011-03-31 21:46:09 -0400

    msm: irqs: add HTC irq

--- arch/arm/mach-msm/include/mach/irqs.h
+++ arch/arm/mach-msm/include/mach/irqs.h
@@ -19,9 +19,13 @@
 
 #define MSM_IRQ_BIT(irq)     (1 << ((irq) & 31))
 
+#define NR_BOARD_IRQS 64
+#define NR_MICROP_IRQS 16
+
 #define FIRST_SIRC_IRQ (NR_MSM_IRQS)
 #define FIRST_GPIO_IRQ (NR_MSM_IRQS + NR_SIRC_IRQS)
 #define FIRST_BOARD_IRQ (NR_MSM_IRQS + NR_SIRC_IRQS + NR_GPIO_IRQS)
+#define FIRST_MICROP_IRQ (FIRST_BOARD_IRQ + NR_BOARD_IRQS)
 
 #if defined(CONFIG_ARCH_MSM7X30)
 #include "irqs-7x30.h"
@@ -36,8 +40,16 @@
 #error "Unknown architecture specification"
 #endif
 
+#if defined(CONFIG_MACH_SUPERSONIC)
+#define NR_IRQS (NR_MSM_IRQS + NR_SIRC_IRQS + NR_GPIO_IRQS + NR_BOARD_IRQS \
+                + NR_MICROP_IRQS)
+#define MSM_INT_TO_GPIO(n) ((n) - NR_MSM_IRQS)
+#define MSM_GPIO_TO_INT(n) (FIRST_GPIO_IRQ + (n))
+#define MSM_uP_TO_INT(n) (FIRST_MICROP_IRQ + (n))
+#else
 #define NR_IRQS (NR_MSM_IRQS + NR_SIRC_IRQS + NR_GPIO_IRQS + NR_BOARD_IRQS)
+#endif
+
 #define MSM_GPIO_TO_INT(n) (FIRST_GPIO_IRQ + (n))
 #define MSM_INT_TO_REG(base, irq) (base + irq / 32)
-
 #endif
