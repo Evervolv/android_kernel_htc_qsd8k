ee2c364c74b1cafa337d93c43b9263cb93a97bd1
Dmitry Shmidt <dimitrysh@google.com>
2011-05-10 16:29:51 -0700

    net: wireless: bcm4329: Add new and default wifi locale support
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- drivers/net/wireless/bcm4329/dhd_custom_gpio.c
+++ drivers/net/wireless/bcm4329/dhd_custom_gpio.c
@@ -183,18 +183,22 @@ dhd_custom_get_mac_address(unsigned char *buf)
 const struct cntry_locales_custom translate_custom_table[] = {
 /* Table should be filled out based on custom platform regulatory requirement */
 #ifdef EXAMPLE_TABLE
+	{"",   "XY",  4}  /* universal */
 	{"US", "US", 69}, /* input ISO "US" to : US regrev 69 */
 	{"CA", "US", 69}, /* input ISO "CA" to : US regrev 69 */
-	{"EU", "EU", 05}, /* input ISO "EU" to : EU regrev 05 */
-	{"FR", "EU", 05},
-	{"DE", "EU", 05},
-	{"IR", "EU", 05},
-	{"UK", "EU", 05}, /* input ISO "UK" to : EU regrev 05 */
-	{"KR", "XY", 03},
-	{"AU", "XY", 03},
-	{"CN", "XY", 03}, /* input ISO "CN" to : XY regrev 03 */
-	{"TW", "XY", 03},
-	{"AR", "XY", 03}
+	{"EU", "EU",  5}, /* input ISO "EU" to : EU regrev 05 */
+	{"FR", "EU",  5},
+	{"DE", "EU",  5},
+	{"IR", "EU",  5},
+	{"UK", "EU",  5}, /* input ISO "UK" to : EU regrev 05 */
+	{"KR", "XY",  3},
+	{"AU", "XY",  3},
+	{"CN", "XY",  3}, /* input ISO "CN" to : XY regrev 03 */
+	{"HK", "XY",  3},
+	{"TW", "XY",  3},
+	{"BR", "XY",  3},
+	{"MX", "XY",  3},
+	{"AR", "XY",  3}
 #endif /* EXAMPLE_TABLE */
 };
 
@@ -219,8 +223,10 @@ void get_customized_country_code(char *country_iso_code, wl_country_t *cspec)
 		if (strcmp(country_iso_code, translate_custom_table[i].iso_abbrev) == 0) {
 			memcpy(cspec->ccode, translate_custom_table[i].custom_locale, WLC_CNTRY_BUF_SZ);
 			cspec->rev = translate_custom_table[i].custom_locale_rev;
-			break;
+			return;
 		}
 	}
+	memcpy(cspec->ccode, translate_custom_table[0].custom_locale, WLC_CNTRY_BUF_SZ);
+	cspec->rev = translate_custom_table[0].custom_locale_rev;
 	return;
 }
