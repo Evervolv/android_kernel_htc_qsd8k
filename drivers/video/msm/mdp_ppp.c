26bfdadcf93d7b15b9ff3c90e4855ca5f7e88106
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-02-02 14:00:12 -0600

    drivers: video: msm: fix build for new pmem

--- drivers/video/msm/mdp_ppp.c
+++ drivers/video/msm/mdp_ppp.c
@@ -717,7 +717,7 @@ static int get_img(struct mdp_img *img, struct fb_info *info,
 
 	return ret;
 }
-
+/*
 static void put_img(struct file *file)
 {
 	if (file) {
@@ -726,6 +726,17 @@ static void put_img(struct file *file)
 		else if (is_msm_hw3d_file(file))
 			put_msm_hw3d_file(file);
 	}
+}
+*/
+void put_img(struct file *p_src_file)
+{
+#ifdef CONFIG_ANDROID_PMEM
+	if (p_src_file)
+		put_pmem_file(p_src_file);
+#else
+    if (is_msm_hw3d_file(file))
+			put_msm_hw3d_file(file);
+#endif
 }
 
 static void dump_req(struct mdp_blit_req *req,
