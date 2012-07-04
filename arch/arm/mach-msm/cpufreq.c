e796747a6d9cf4c6232bb9fb1f019e8e9ff734c8
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-07-03 21:13:25 -0500

    msm: Add sysfs interface for mfreq
    Add mfreq interface in sysfs.  This forces the system to use the
    maximum CPU clock on all CPU cores.
    (mfreq=1 enables, mfreq=0 disables)
    
    *other misc cleanup

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -3,7 +3,7 @@
  * MSM architecture cpufreq driver
  *
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2007 QUALCOMM Incorporated
+ * Copyright (c) 2007-2010, Code Aurora Forum. All rights reserved.
  * Author: Mike A. Chan <mikechan@google.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -17,17 +17,42 @@
  *
  */
 
+#include <linux/cpu.h>
 #include <linux/cpufreq.h>
 #include <linux/earlysuspend.h>
 #include <linux/init.h>
+#include <linux/sysdev.h>
 #include "acpuclock.h"
+
+static int override_cpu;
+
+static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq)
+{
+	int ret = 0;
+	struct cpufreq_freqs freqs;
+
+	freqs.old = policy->cur;
+	if (override_cpu) {
+		if (policy->cur == policy->max)
+			return 0;
+		else
+			freqs.new = policy->max;
+	} else
+		freqs.new = new_freq;
+	freqs.cpu = policy->cpu;
+	cpufreq_notify_transition(&freqs, CPUFREQ_PRECHANGE);
+	ret = acpuclk_set_rate(new_freq * 1000, 0);
+	if (!ret)
+		cpufreq_notify_transition(&freqs, CPUFREQ_POSTCHANGE);
+
+	return ret;
+}
 
 static int msm_cpufreq_target(struct cpufreq_policy *policy,
 				unsigned int target_freq,
 				unsigned int relation)
 {
 	int index;
-	struct cpufreq_freqs freqs;
 	struct cpufreq_frequency_table *table =
 		cpufreq_frequency_get_table(policy->cpu);
 
@@ -41,16 +66,12 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 		return 0;
 
 #ifdef CONFIG_CPU_FREQ_DEBUG
-	printk("msm_cpufreq_target %d r %d (%d-%d) selected %d\n", target_freq,
-		relation, policy->min, policy->max, table[index].frequency);
+	pr_debug("CPU[%d] target %d relation %d (%d-%d) selected %d\n",
+		policy->cpu, target_freq, relation,
+		policy->min, policy->max, table[index].frequency);
 #endif
-	freqs.old = policy->cur;
-	freqs.new = table[index].frequency;
-	freqs.cpu = policy->cpu;
-	cpufreq_notify_transition(&freqs, CPUFREQ_PRECHANGE);
-	acpuclk_set_rate(table[index].frequency * 1000, 0);
-	cpufreq_notify_transition(&freqs, CPUFREQ_POSTCHANGE);
-	return 0;
+
+	return set_cpu_freq(policy, table[index].frequency);
 }
 
 static int msm_cpufreq_verify(struct cpufreq_policy *policy)
@@ -68,13 +89,32 @@ static int msm_cpufreq_init(struct cpufreq_policy *policy)
 	BUG_ON(cpufreq_frequency_table_cpuinfo(policy, table));
 	policy->cur = acpuclk_get_rate();
 #ifdef CONFIG_MSM_CPU_FREQ_SET_MIN_MAX
-        policy->min = CONFIG_MSM_CPU_FREQ_MIN;
-        policy->max = CONFIG_MSM_CPU_FREQ_MAX;
+	policy->min = CONFIG_MSM_CPU_FREQ_MIN;
+	policy->max = CONFIG_MSM_CPU_FREQ_MAX;
 #endif
 	policy->cpuinfo.transition_latency =
 		acpuclk_get_switch_time() * NSEC_PER_USEC;
 	return 0;
 }
+
+static ssize_t store_mfreq(struct sysdev_class *class,
+			struct sysdev_class_attribute *attr,
+			const char *buf, size_t count)
+{
+	u64 val;
+
+	if (strict_strtoull(buf, 0, &val) < 0) {
+		pr_err("Invalid parameter to mfreq\n");
+		return 0;
+	}
+	if (val)
+		override_cpu = 1;
+	else
+		override_cpu = 0;
+	return count;
+}
+
+static SYSDEV_CLASS_ATTR(mfreq, 0200, NULL, store_mfreq);
 
 static struct freq_attr *msm_cpufreq_attr[] = {
 	&cpufreq_freq_attr_scaling_available_freqs,
@@ -93,7 +133,12 @@ static struct cpufreq_driver msm_cpufreq_driver = {
 
 static int __init msm_cpufreq_register(void)
 {
+	int err = sysfs_create_file(&cpu_sysdev_class.kset.kobj,
+			&attr_mfreq.attr);
+	if (err)
+		pr_err("Failed to create sysfs mfreq\n");
+
 	return cpufreq_register_driver(&msm_cpufreq_driver);
 }
 
-device_initcall(msm_cpufreq_register);
+late_initcall(msm_cpufreq_register);
