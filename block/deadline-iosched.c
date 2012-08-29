cdcf3f82d6330471ea048e9334145bc7bafccc86
faux123 <reioux@gmail.com>
2012-08-28 19:06:59 -0500

    block/deadline: tweaked for better performance on android

--- block/deadline-iosched.c
+++ block/deadline-iosched.c
@@ -17,9 +17,9 @@
 /*
  * See Documentation/block/deadline-iosched.txt
  */
-static const int read_expire = HZ / 2;  /* max time before a read is submitted. */
+static const int read_expire = HZ / 4;  /* max time before a read is submitted. */
 static const int write_expire = 5 * HZ; /* ditto for writes, these limits are SOFT! */
-static const int writes_starved = 2;    /* max times reads can starve a write */
+static const int writes_starved = 4;    /* max times reads can starve a write */
 static const int fifo_batch = 1;       /* # of sequential requests treated as one
 				     by the above parameters. For throughput. */
 
