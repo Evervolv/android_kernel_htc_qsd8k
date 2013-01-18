d196ad5edcebe125d624067e19337f9533d46a4d
marc1706 <admin@m-a-styles.de>
2013-01-18 19:37:41 +0100

    socinfo: add dummy info for htcleo
    Signed-off-by: marc1706 <admin@m-a-styles.de>

--- arch/arm/mach-msm/socinfo.c
+++ arch/arm/mach-msm/socinfo.c
@@ -588,6 +588,12 @@ arch_initcall(socinfo_init_sysdev);
 
 void *setup_dummy_socinfo(void)
 {
+#ifdef CONFIG_MACH_HTCLEO
+
+	dummy_socinfo.id = 30; // look above: [30] = MSM_CPU_8X50,
+	return (void *) &dummy_socinfo;
+
+#else
 	if (machine_is_msm8960_rumi3() || machine_is_msm8960_sim() ||
 	    machine_is_msm8960_cdp())
 		dummy_socinfo.id = 87;
@@ -596,6 +602,7 @@ void *setup_dummy_socinfo(void)
 	else if (machine_is_msm9615_mtp() || machine_is_msm9615_cdp())
 		dummy_socinfo.id = 104;
 	return (void *) &dummy_socinfo;
+#endif
 }
 
 int __init socinfo_init(void)
